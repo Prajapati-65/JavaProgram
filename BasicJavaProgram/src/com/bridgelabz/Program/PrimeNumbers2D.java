c68e42e0e4031f2d4ddaa24fe65a629e1535b86f
om65praj <om65praj@gmail.com>
2017-09-07 18:42:59 +0530

    new commit

--- BasicJavaProgram/src/com/bridgelabz/Program/PrimeNumbers2D.java
+++ BasicJavaProgram/src/com/bridgelabz/Program/PrimeNumbers2D.java
@@ -12,25 +12,24 @@ public class PrimeNumbers2D {
 		Utility utility = new Utility();
 		System.out.println("Enter a number :");
 		int number = utility.inputInteger();
-<<<<<<< HEAD
 		String[] string1 = utility.checkPrime(number);
-		
-		String[][] string2 = new String[number][number];	
-		
+		String[][] string2 = new String[number][number];
 		for (int i = 0; i < string2.length; i++) {
 			for (int j = 0; j < string2[i].length; j++) {
-				string2[i][j] = string1[(j*string2.length) + i];
+				string2[i][j] = string1[(j * string2.length) + i];
 				System.out.print(string2[i][j] + " ");
-=======
-		String[][] str1 = new String[number][number];
-		String[] str2 = utility.checkPrime(number);
-		for (int i = 0; i < str1.length; i++) {
-			for (int j = 0; j < str1[i].length; j++) {
-				str1[i][j] = str2[j];
-				System.out.print(str1[i][j] + " ");
->>>>>>> 9663d15798b30a845e99e0f8b435ec678ca3a968
+				String[][] str1 = new String[number][number];
+				String[] str2 = utility.checkPrime(number);
+				for (int n = 0; n < str1.length; n++)
+				{
+					for (int m = 0; m < str1[n].length; m++)
+					{
+						str1[n][m] = str2[m];
+						System.out.print(str1[n][m] + " ");
+					}
+					System.out.println();
+				}
 			}
-			System.out.println();
 		}
 	}
 }
