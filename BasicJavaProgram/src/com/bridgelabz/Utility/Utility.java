9a024d852c2a75058a7f8403558b63c1231b29e5
om65praj <om65praj@gmail.com>
2017-09-07 19:09:55 +0530

    new commit

--- BasicJavaProgram/src/com/bridgelabz/Utility/Utility.java
+++ BasicJavaProgram/src/com/bridgelabz/Utility/Utility.java
@@ -148,13 +148,14 @@ public class Utility {
 		}
 		return true;
 	}
-	
-	public String[] checkPrime(int limit)
-	{
+	/**
+	 * @param input limit
+	 * @return create a method to String[] prime
+	 */
+	public String[] checkPrime(int limit) {
 		String[] arry = new String[limit];
 		int position=0;
-		for(int i=2; i < limit; i++)
-		{
+		for(int i=2; i < limit; i++) {
              if(isPrime(i)){
             	arry[position] = String.valueOf(i);
 				position++;
@@ -162,33 +163,11 @@ public class Utility {
 		}
         String[] array1 = new String[position];
  		for (int k = 0; k < position; k++) {
- 			
  			array1[k] = arry[k];
  		}
  		return array1;      
 	}
 	
-
-	/**
-	 * @param input limit
-	 * @return create a method to String[] prime
-	 */
-	public String[] checkPrime(int limit) {
-		String[] arry = new String[limit];
-		int position = 0;
-		for (int i = 2; i < limit; i++) {
-			if (isPrime(i)) {
-				arry[position] = String.valueOf(i);
-				position++;
-			}
-		}
-		String[] array1 = new String[position];
-		for (int k = 0; k < position; k++) {
-
-			array1[k] = arry[k];
-		}
-		return array1;
-	}
 
 	/**
 	 * @param input number
@@ -214,26 +193,20 @@ public class Utility {
 	 * @param  create a method to Anagram
 	 * @return boolean
 	 */
-	public boolean checkAnagram(String string1, String string2) {
+	public boolean checkAnagram(String string1, String string2)
+	{
 		if (string1.length() != string2.length()) {
 			return false;
 		}
-<<<<<<< HEAD
 		char[] charArray1 = string1.toCharArray();
-=======
-		char[] charArray1= string1.toCharArray();
->>>>>>> 9663d15798b30a845e99e0f8b435ec678ca3a968
 		Arrays.sort(charArray1);
 		char[] charArray2 = string2.toCharArray();
 		Arrays.sort(charArray2);
 		for (int i = 0; i < charArray1.length; i++) {
-<<<<<<< HEAD
 			if (charArray1[i] != charArray2[i]) {
-=======
-			if(charArray1[i]!=charArray2[i])
-			{
->>>>>>> 9663d15798b30a845e99e0f8b435ec678ca3a968
-				return false;
+				if(charArray1[i]!=charArray2[i]) {
+					return false;
+				}
 			}
 		}
 		return true;
@@ -243,23 +216,12 @@ public class Utility {
 	 * @param input number
 	 * @return String Array
 	 */
-<<<<<<< HEAD
-=======
-
-<<<<<<< HEAD
->>>>>>> 151bd8227db993dbc42c6c8aa915dcb4590ecd1c
-	public String[] prime(int number) {
-		String[] array = new String[1000];
-		int position = 0;
-		for (int i = 2; i <= number; i++) {
-=======
 	public String[] prime(int number) 
 	{
 		String[] array = new String[1000];
 		int position = 0;
 		for (int i = 2; i <= number; i++)
 		{
->>>>>>> 9663d15798b30a845e99e0f8b435ec678ca3a968
 			boolean b = true;
 			for (int j = 2; j <= i / 2; j++) 
 			{
@@ -281,11 +243,9 @@ public class Utility {
 		return array1;
 	}
 
-	/*
-	 * create a method to check and print Palindrome number
-	 */
 	/**
 	 * @param input String 
+	 * @param create a method to check and print Palindrome number
 	 * @return boolean
 	 */
 	public boolean palindrome(String s) {
